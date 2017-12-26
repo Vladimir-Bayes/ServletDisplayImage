1c88bf543dd90309cdc0e55a3b282747f773d371
Vladimir-Bayes <gyang.shines@gmail.com>
2017-12-26 17:19:37 +0800

    Initial commit

---
+++ src/com/yiibai/DisplayImage.java
@@ -0,0 +1,50 @@
+package com.yiibai;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DisplayImage
+ */
+@WebServlet("/DisplayImage")
+public class DisplayImage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("image/jpeg");
+		ServletOutputStream out;
+		out = response.getOutputStream();
+		String filepath = request.getSession().getServletContext().getRealPath("");
+		
+		System.out.println(filepath);
+		System.out.println(filepath+"java.jpg");
+		
+		FileInputStream fin = new FileInputStream(filepath+"java.jpg");
+
+		BufferedInputStream bin = new BufferedInputStream(fin);
+		BufferedOutputStream bout = new BufferedOutputStream(out);
+		int ch = 0;
+		;
+		while ((ch = bin.read()) != -1) {
+			bout.write(ch);
+		}
+
+		bin.close();
+		fin.close();
+		bout.close();
+		out.close();
+	}
+
+}
